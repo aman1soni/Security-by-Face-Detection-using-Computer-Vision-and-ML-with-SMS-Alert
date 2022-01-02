917472fa200a5c853718bf857f1f34696a85c507
Aman soni <55807127+aman1soni@users.noreply.github.com>
2022-01-02 23:14:01 +0530

    Update sms.py
    some updated related to api key

--- sms.py
+++ sms.py
@@ -4,7 +4,7 @@ import json
 def sendsms():
     url = 'https://www.fast2sms.com/dev/bulk'
     params = {
-            'authorization': 'noyaAWs9wUgpJtGvhK50PN8r1dY3CiEqTkbFMeS7OR2ZjxQ4ufW9FG8NcOCfX35yLS1d0ta4rewVsBIz',
+            'authorization': '//YOUR_FAST2SMS_API_KEY',
             'sender_id': 'FSTSMS',
             'message': "Alert! Someone aunthenticated user try to access your personal computer.\n\nMessage sent by Admin.\n\nThanks",
             'language': 'english',
